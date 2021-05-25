ed1ddcd24503fdfb78153ed17f1a8580ee1d890c
Paulo Calanca <sir.calanca@gmail.com>
2021-05-25 12:39:26 -0300

    final

--- Exercicios/Adivinhação.py
+++ Exercicios/Adivinhação.py
@@ -3,15 +3,19 @@ print(f"{'~'*5}Bem vindo ao jogo da adivinhação!!{'~'*5}")
 print(f"{'~'*45}")
 
 
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+acerto = chute == numero_secreto
+
 numero_secreto = 42
 chute = float(input("Digite o seu numero: "))
 print("Voce digitou", chute)
-if(numero_secreto == chute):
+if(numero_secreto == acerto):
     print('Você acertou !!')
 else:
-    if(chute > numero_secreto):
+    if(chute > maior):
         print("Voce errou! O seu chute foi maior do que o numero secreto")
-    elif (chute < numero_secreto):
+    elif (menor):
         print("Você errou!!O seu chute foi menor do que o numero secreto")
 
 print("Fim de game.")
