e614419ed649a10dc8f2849262e08fda6d0602db
Paulo Calanca <sir.calanca@gmail.com>
2021-05-27 09:08:21 -0300

    limitando chute

--- Exercicios/Adivinhação(3.0).py
+++ Exercicios/Adivinhação(3.0).py
@@ -9,13 +9,15 @@ total_tentativas= 3
 
 for rodada in range(1, total_tentativas + 1):
     print("Tentativas {} de {}".format(rodada,total_tentativas))
-    chute = float(input("Digite o seu numero:\n "))
+    chute = float(input("Digite o seu número entre 1 ate 100:\n "))
     maior = chute > numero_secreto
     menor = chute < numero_secreto
     acerto = chute == numero_secreto
 
     print("Voce digitou", chute)
-
+    if(chute < 1 or chute > 100):
+        print("Você deveria digitar um numero valido!!")
+        continue
     if(acerto):
         print('Você acertou !!\n')
         break
